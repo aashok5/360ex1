88637f1a8872ecafcb9941e064ffb88f0855d1f3
aashok5 <72227120+aashok5@users.noreply.github.com>
2020-10-05 21:14:31 -0700

    Update AddingMachine.java

--- AddingMachine.java
+++ AddingMachine.java
@@ -2,25 +2,34 @@ package cse360assignment02;
 
 public class AddingMachine {
   private int total;
+  private string transactions = "";
   
   public AddingMachine () {
     total = 0;  // not needed - included for clarity
+    transactions="0";
   }
   
   public int getTotal () {
-    return 0;
+    //return 0;
+    return total;
   }
   
   public void add (int value) {
+    total = total + value;
+    string+=" + "+value;
   }
 
   public void subtract (int value) {
+    total = total - value
+    string+=" - "+value;  
   }
 
   public String toString () {
-    return "";
+    return transactions;
   }
 
   public void clear() {
+      transactions = "0";
+      total = 0;
   }
 }
