107ccf9c862eb8a5cedcaed7c8920e1f7d422da7
aashok5 <72227120+aashok5@users.noreply.github.com>
2020-10-05 21:57:53 -0700

    Added comments

--- AddingMachine.java
+++ AddingMachine.java
@@ -2,29 +2,35 @@ package cse360assignment02;
 
 public class AddingMachine {
   private int total;
-  private string transactions = "";
+  //Creating the transaction string
+  private string transactions;
   
   public AddingMachine () {
     total = 0;  // not needed - included for clarity
+    //Initializing string
     transactions="0";
   }
   
   public int getTotal () {
     //return 0;
+    // Returning the total value
     return total;
   }
   
   public void add (int value) {
+    //Adding parameter to total variable
     total = total + value;
     string+=" + "+value;
   }
 
   public void subtract (int value) {
+    //Subtract parameter from total variable
     total = total - value
     string+=" - "+value;  
   }
 
   public String toString () {
+    // Returning the transactions
     return transactions;
   }
 
---
+++ test.java
@@ -0,0 +1,13 @@
+package cse360assignment02;
+
+public class test{
+	public static void main(String args[])
+	{
+
+		test ass = new test();
+		ass.add(4);
+		ass.subtract(5);
+		ass.add(6);
+
+	}
+}
